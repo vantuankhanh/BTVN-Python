9fdf7e6d430e3c892e2982226e28da510418335b
khanh <vantuankhanh09@gmail.com>
2023-01-08 23:37:32 +0700

    them file

--- Sắp xếp các số nguyên.py
+++ Sắp xếp các số nguyên.py
@@ -1,15 +1,9 @@
-def sapxep(num1,num2,num3):
-    max1=num1
-    a=[num1,num2,num3]
+def find_min(a):
+    min1=a[0]
     for i in a:
-        if i>max1:
-            max1=i
-    a.remove(max1)
-    if a[0]<a[1]:
-        a[0],a[1]=a[1],a[0]
-    return [max1,a[0],a[1]]
-x = int(input("Enter first number: "))
-y = int(input("Enter second number: "))
-z = int(input("Enter third number: "))
-print(sapxep(x,y,z))
+        if i<min1:
+            min1=i
+    return min1
+num=[1129,254,123,6231,6,123,6,123231,6574,235,4581]
+print(find_min(num))
             
